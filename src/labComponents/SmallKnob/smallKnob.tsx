033261b1f6a429540ca314e68a3845459f23e1e4
Milad Haghighi <milhaghighi@gmail.com>
2024-10-15 12:04:03 -0700

    added 2 small knobs and 2 different buttons then move when clicked
    added 2 small knobs and 2 different buttons that move when clicked

---
+++ src/labComponents/SmallKnob/smallKnob.tsx
@@ -0,0 +1,42 @@
+import React, { useRef, useEffect} from 'react';
+import * as THREE from 'three';
+import { useLoader } from '@react-three/fiber';
+import { PLYLoader } from 'three-stdlib';
+
+interface SmallKnobProps {
+  position: [number, number, number]; // Position prop for placement in the scene
+  rotation :[number, number, number];
+  onClick?: () => void;
+}
+
+
+
+const SmallKnob: React.FC<SmallKnobProps> = ({position, rotation, onClick}) => {
+    const dialRef = useRef<THREE.Mesh>(null!); // Using a ref for the needle
+    
+    
+    useEffect(() => {
+        const loader1 = new PLYLoader();
+        loader1.load('/smallKnob.ply', (geometry) => {
+          geometry.computeVertexNormals();
+          if (dialRef.current) {
+            dialRef.current.geometry = geometry; // Set the loaded geometry for the needle
+          }
+        });
+      }, []);  
+    const groupRef = useRef<THREE.Group | null>(null);
+
+    return (
+      <group ref={groupRef} position={position} rotation={rotation}>
+
+        <mesh ref={dialRef} scale={[0.1, 0.1, 0.1]} onClick={onClick}>
+            
+            <meshStandardMaterial color={0x808080} side={THREE.DoubleSide}/>
+
+        </mesh>
+      
+    </group>
+  );
+};
+
+export default SmallKnob;
