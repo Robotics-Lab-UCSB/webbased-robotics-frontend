1ad68f3375118d6945201d6a681dc5db289b4bf7
Makkerel <kzhao132018@gmail.com>
2024-10-26 00:17:42 -0700

    added scrolling

--- src/Assisstant/chatbox.tsx
+++ src/Assisstant/chatbox.tsx
@@ -21,46 +21,48 @@ const ChatBox: React.FC<ChatBoxProps> = ({ color, t_width, t_height, isOpen, tog
     if (inputText.trim() !== '') {
       setMessages([...messages, inputText]); // Add the new message to the list
       setInputText(''); // Clear the input field
+      
     }
   };
 
-
   return (
-    <Draggable handle=".drag-handle">
-      <div
-        className={`chat-box ${isOpen ? 'open' : ''}`}
-        style={{ backgroundColor: color, width: t_width, height: t_height }}
-      >
-        <div className="drag-handle">
-          <div className="close-btn" onClick={toggleChatBox}>
-            X
-          </div>
-        </div>
-        {isOpen && (
-          <div className="content">
-            {messages.map((message, index) => (
-              <MessageBubble
-              id={index}
-              text={message}
-              onClick={onMessageClick}
-              />
-            ))}
-            <div className="input-area">
-              <input
-                type="text"
-                value={inputText}
-                onChange={(e) => setInputText(e.target.value)}
-                placeholder="Type a message..."
-                className="chat-input"
-              />
-              <button onClick={SendMessage} className="send-btn">
-                Send
-              </button>
+      <Draggable handle=".drag-handle">
+          <div
+            className={`chat-box ${isOpen ? 'open' : ''}`}
+            style={{ backgroundColor: color, width: t_width, height: t_height}}
+          >
+            <div className="drag-handle">
+              <div className="close-btn" onClick={toggleChatBox}>
+                X
+              </div>
             </div>
+            {isOpen && (
+              <div className='contentAndInput'>
+                <div className="content">
+                  {messages.map((message, index) => (
+                    <MessageBubble
+                    id={index}
+                    text={message}
+                    onClick={onMessageClick}
+                    />
+                  ))}
+                  </div>
+                <div className="input-area">
+                <input
+                  type="text"
+                  value={inputText}
+                  onChange={(e) => setInputText(e.target.value)}
+                  placeholder="Type a message..."
+                  className="chat-input"
+                />
+                <button onClick={SendMessage} className="send-btn">
+                  Send
+                </button>
+              </div>
+              </div>
+            )}
           </div>
-        )}
-      </div>
-    </Draggable>
+      </Draggable>
   );
 };
 
